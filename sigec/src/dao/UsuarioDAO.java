e332b909ebfdbd13accae87aec41d29440b1a701
CLEVERSON <cleverson.silva@msn.com>
2026-02-28 17:39:59 -0400

    Iniciando a contrução de Usuário DAO

---
+++ sigec/src/dao/UsuarioDAO.java
@@ -0,0 +1,31 @@
+
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import model.Usuario;
+import util.ConexaoBD;
+
+public class UsuarioDAO {
+    public static boolean cadastrarUsuarioDAO(Usuario usuario){
+        Connection conexao = ConexaoBD.conectar();
+        PreparedStatement pstmt = null;
+        try {
+            String sql = "INSERT INTO usuario(nome_completo, login, senha, codigo_Perfil_de_Acesso) VALUES (?, ?, ?, ?)";
+            pstmt = conexao.prepareStatement(sql);
+            pstmt.setString(1, usuario.getNome());
+            pstmt.setString(2, usuario.getLogin());
+            pstmt.setString(3, usuario.getSenha());
+            pstmt.setInt(4, usuario.getPerfilAcesso().getIdPerfilAcesso());
+            
+            int linhasAfetadas = pstmt.executeUpdate();
+            return linhasAfetadas > 0;
+        } catch (SQLException e) {
+            System.out.println("Erro ao cadastrar o novo usuário!" + e.getMessage());
+            return false;
+        }finally{
+            ConexaoBD.fecharConexao(conexao, pstmt);
+        }
+    }
+}
