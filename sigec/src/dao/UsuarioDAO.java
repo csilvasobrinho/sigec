c6b2e5387553c8773f35913d717a8d507cf4cf06
CLEVERSON <cleverson.silva@msn.com>
2026-03-04 18:18:58 -0400

    .

--- sigec/src/dao/UsuarioDAO.java
+++ sigec/src/dao/UsuarioDAO.java
@@ -31,6 +31,46 @@ public class UsuarioDAO {
             ConexaoBD.fecharConexao(conexao, pstmt);
         }
     }
+    public static boolean atualizarUsuarioDAO(Usuario usuario){
+        Connection conexao = ConexaoBD.conectar();
+        PreparedStatement pstmt = null;
+        try {
+            String sql = "INSERT INTO usuario(nome_completo, login, senha, codigo_Perfil_de_Acesso) VALUES (?, ?, ?, ?)";
+            pstmt = conexao.prepareStatement(sql);
+            pstmt.setString(1, usuario.getNome());
+            pstmt.setString(2, usuario.getLogin());
+            pstmt.setString(3, usuario.getSenha());
+            pstmt.setInt(4, usuario.getPerfilAcesso().getIdPerfilAcesso());
+            
+            int linhasAfetadas = pstmt.executeUpdate();
+            return linhasAfetadas > 0;
+        } catch (SQLException e) {
+            System.out.println("Erro ao cadastrar o novo usuário!" + e.getMessage());
+            return false;
+        }finally{
+            ConexaoBD.fecharConexao(conexao, pstmt);
+        }
+    }
+    public static boolean deletarUsuarioDAO(Usuario usuario){
+        Connection conexao = ConexaoBD.conectar();
+        PreparedStatement pstmt = null;
+        try {
+            String sql = "INSERT INTO usuario(nome_completo, login, senha, codigo_Perfil_de_Acesso) VALUES (?, ?, ?, ?)";
+            pstmt = conexao.prepareStatement(sql);
+            pstmt.setString(1, usuario.getNome());
+            pstmt.setString(2, usuario.getLogin());
+            pstmt.setString(3, usuario.getSenha());
+            pstmt.setInt(4, usuario.getPerfilAcesso().getIdPerfilAcesso());
+            
+            int linhasAfetadas = pstmt.executeUpdate();
+            return linhasAfetadas > 0;
+        } catch (SQLException e) {
+            System.out.println("Erro ao cadastrar o novo usuário!" + e.getMessage());
+            return false;
+        }finally{
+            ConexaoBD.fecharConexao(conexao, pstmt);
+        }
+    }
     public static ArrayList<Usuario> listaUsuarioDAO(){
         Connection conexao = ConexaoBD.conectar();
         PreparedStatement pstmt = null;
@@ -41,8 +81,7 @@ public class UsuarioDAO {
             pstmt = conexao.prepareStatement(sql);
             ResultSet rs = null;
         } catch (Exception e) {
+            return false;
         }
-        
     }
-    
 }
